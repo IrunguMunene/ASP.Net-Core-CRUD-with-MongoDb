c34955d702aacbdfb0bebff694ce8eee4e150dd9
Francis Irungu Munene <Irungu.Munene@gmail.com>
2020-09-07 17:20:27 +0300

    Changed NavMenu to a functional companent.

--- MongoCrudUi/ClientApp/src/components/NavMenu.jsx
+++ MongoCrudUi/ClientApp/src/components/NavMenu.jsx
@@ -3,47 +3,31 @@ import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem } from
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
+export const NavMenu = () => {
     return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <NavbarBrand tag={Link} to="/">Secure Privacy Code Test</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                    <Link to={'/CreateEmployee'} className="nav-link text-dark">Add Employee</Link>
-                </NavItem>
-                <NavItem>
-                  <Link to={'/EmployeeList'} className="nav-link">Employees</Link>
-                </NavItem>
-                 <NavItem>
-                  <Link to={'/GoodBinaryStringChecker'} className="nav-link">Task 2 Solution</Link>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <NavItem>
+                        <Link to={'/CreateEmployee'} className="nav-link text-dark">Add Employee</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to={'/EmployeeList'} className="nav-link">Employees</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to={'/GoodBinaryStringChecker'} className="nav-link">Task 2 Solution</Link>
+                    </NavItem>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
     );
-  }
 }
