36e525e5a5e4a0394a5171d265582d6b5b089257
Francis Irungu Munene <Irungu.Munene@gmail.com>
2020-09-07 17:23:13 +0300

    Removed search - to be implemented later.

--- MongoCrudUi/ClientApp/src/components/NavMenu.jsx
+++ MongoCrudUi/ClientApp/src/components/NavMenu.jsx
@@ -23,10 +23,6 @@ export const NavMenu = () => {
                         <Link to={'/GoodBinaryStringChecker'} className="nav-link">Task 2 Solution</Link>
                     </NavItem>
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
     );
