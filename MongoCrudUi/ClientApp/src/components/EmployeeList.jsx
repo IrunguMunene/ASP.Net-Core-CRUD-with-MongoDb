ca8796a1a45a84edaa9e16b39b9d5041f8c90fbe
Francis Irungu Munene <Irungu.Munene@gmail.com>
2020-09-08 08:45:23 +0300

    Styling Changes.

--- MongoCrudUi/ClientApp/src/components/EmployeeList.jsx
+++ MongoCrudUi/ClientApp/src/components/EmployeeList.jsx
@@ -11,27 +11,29 @@ export const EmployeeList = () => {
     }
 
     return (
-        <div className="row table-responsive">
+        <div>
             <Filter />
-            <table className="table table-bordered table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Employee Name</th>
-                        <th>Gender</th>
-                        <th>Department</th>
-                        <th>Designation</th>
-                        <th>Age</th>
-                        <th>City</th>
-                        <th>Country</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(employee => (
-                        <TableRow key={employee.id} employee={employee} />
-                    ))}
-                </tbody>
-            </table>
+            <div className="row table-responsive-sm">
+                <table className="table table-bordered table-striped table-sm">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>Employee Name</th>
+                            <th>Gender</th>
+                            <th>Department</th>
+                            <th>Designation</th>
+                            <th>Age</th>
+                            <th>City</th>
+                            <th>Country</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map(employee => (
+                            <TableRow key={employee.id} employee={employee} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- MongoCrudUi/ClientApp/src/components/NavMenu.jsx
+++ MongoCrudUi/ClientApp/src/components/NavMenu.jsx
@@ -5,24 +5,26 @@ import './NavMenu.css';
 
 export const NavMenu = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <NavbarBrand tag={Link} to="/">Secure Privacy Code Test</NavbarBrand>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <nav className="navbar navbar-default navbar-expand-lg navbar-dark bg-primary navbar-fixed-top">
+            <div className="container-fluid">
+                <NavbarBrand tag={Link} to="/">Secure Privacy Code Test</NavbarBrand>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul className="navbar-nav">
-                    <NavItem>
-                        <Link to={'/CreateEmployee'} className="nav-link text-dark">Add Employee</Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to={'/EmployeeList'} className="nav-link">Employees</Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to={'/GoodBinaryStringChecker'} className="nav-link">Task 2 Solution</Link>
-                    </NavItem>
-                </ul>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                        <NavItem>
+                            <Link to={'/CreateEmployee'} className="nav-link text-dark">Add Employee</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to={'/EmployeeList'} className="nav-link">Employees</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to={'/GoodBinaryStringChecker'} className="nav-link">Task 2 Solution</Link>
+                        </NavItem>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
