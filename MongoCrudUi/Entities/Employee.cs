980b356159986f870056316ab2f03af066b1e869
Francis Irungu Munene <Irungu.Munene@gmail.com>
2020-09-12 17:59:58 +0300

    Added BsonRequired attribute

--- MongoCrudUi/Entities/Employee.cs
+++ MongoCrudUi/Entities/Employee.cs
@@ -9,24 +9,31 @@ namespace MongoCrudUi.Entities
     {
         #region Properties
         [BsonElement("name")]
+        [BsonRequired]
         public string Name { get; set; }
 
         [BsonElement("department")]
+        [BsonRequired]
         public string Department { get; set; }
 
         [BsonElement("designation")]
+        [BsonRequired]
         public string Designation { get; set; }
 
         [BsonElement("age")]
+        [BsonRequired]
         public int Age { get; set; }
 
         [BsonElement("city")]
+        [BsonRequired]
         public string City { get; set; }
 
         [BsonElement("country")]
+        [BsonRequired]
         public string Country { get; set; }
 
         [BsonElement("gender")]
+        [BsonRequired]
         public string Gender { get; set; }
         #endregion
     }
